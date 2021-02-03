f817dcec36cea3f93efdbfe57efa1f6681892b9f
Ray Nimalan <raymondnimalan@gmail.com>
2021-02-03 17:57:00 -0500

    feat(python integration): added route to run python scripts

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
+const {spawn} = require('child_process')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -38,6 +39,28 @@ router.get('/', async (req, res, next) => {
       console.log(response.payload)
     }
     res.send('Analysis Complete!')
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/python', (req, res, next) => {
+  try {
+    let dataToSend
+    // spawn new child process to call the python script
+    const python = spawn('python', ['./server/api/script1.py'])
+    // collect data from script
+    python.stdout.on('data', function(data) {
+      console.log('Pipe data from python script ...')
+      dataToSend = data.toString()
+    })
+    // in close event we are sure that stream from child process is closed
+    python.on('close', code => {
+      console.log(`child process close all stdio with code ${code}`)
+      // send data to browser
+      res.send(dataToSend)
+      console.log(dataToSend)
+    })
   } catch (err) {
     next(err)
   }
