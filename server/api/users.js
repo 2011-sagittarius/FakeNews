649b99cc5cb3915979b9dd511874ff93265f1977
j-dags <jon.dagdagan@gmail.com>
2021-02-04 11:19:23 -0600

    feat: add preprocess.py

--- server/api/users.js
+++ server/api/users.js
@@ -48,7 +48,38 @@ router.get('/python', (req, res, next) => {
   try {
     let dataToSend
     // spawn new child process to call the python script
-    const python = spawn('python', ['./server/api/script1.py'])
+    const python = spawn('python3', ['./server/api/script1.py'])
+    // collect data from script
+    python.stdout.on('data', function(data) {
+      console.log('Pipe data from python script ...')
+      dataToSend = data.toString()
+    })
+    // in close event we are sure that stream from child process is closed
+    python.on('close', code => {
+      console.log(`child process close all stdio with code ${code}`)
+      // send data to browser
+      res.send(dataToSend)
+      console.log(dataToSend)
+    })
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/python/preprocess', (req, res, next) => {
+  try {
+    let dataToSend
+    // spawn new child process to call the python script
+    const python = spawn('python3', [
+      './python/PreProcess.py',
+      'pie-thawn is da bes'
+    ])
+    // const python = spawn('python3', [
+    //   './python/Params.py',
+    //   'node.js',
+    //   'pie-thawn',
+    // ])
+
     // collect data from script
     python.stdout.on('data', function(data) {
       console.log('Pipe data from python script ...')
