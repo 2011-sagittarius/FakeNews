785c918bc047fc9e0f083dca06b851130203486c
j-dags <jon.dagdagan@gmail.com>
2021-02-08 10:59:43 -0600

    add keyword extraction script

--- server/api/processing.js
+++ server/api/processing.js
@@ -83,7 +83,37 @@ router.get('/scrape', (req, res) => {
     if (response.statusCode === 200) {
       let data = response.json.body.content
       // console.log(response.json.body.content)
+      console.log('scrape res > ', response.json)
       res.send(data)
     }
   })
+})
+
+// Get related news articles
+// Python script to preprocess aka remove filler words/characters from text body
+router.get('/related-articles', async (req, res, next) => {
+  try {
+    let dataToSend
+    // console.log('req.query > ', req.query.text)
+    // spawn new child process to call the python script
+    const python = await spawn('python3', [
+      './python/PreProcess.py',
+      req.query.text
+    ])
+
+    // collect data from script
+    python.stdout.on('data', function(data) {
+      console.log('Pipe data from python script ...')
+      dataToSend = data.toString()
+    })
+    // in close event we are sure that stream from child process is closed
+    python.on('close', code => {
+      console.log(`child process close all stdio with code ${code}`)
+      // send data to browser
+      res.send(dataToSend)
+      console.log(dataToSend)
+    })
+  } catch (err) {
+    next(err)
+  }
 })
